34fc337f7bfcbac03de21d129ae7e8b9575a1811
Viliam Pucik <viliampucik@users.noreply.github.com>
2019-12-10 23:34:43 +0100

    10b

--- 2019/10.py
+++ 2019/10.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 import fileinput
-from cmath import phase
+from cmath import polar, pi
+
+
+def rotate(phi):  # +90 degrees rotation
+    phi -= 3 * pi / 2
+    if phi < -pi:
+        phi += 2 * pi
+    return phi
+
 
 a = {}
 y = 0
@@ -8,12 +16,36 @@ y = 0
 for line in fileinput.input():
     for x, v in enumerate(line.strip()):
         if v == "#":
-            a[complex(x, y)] = set()
+            a[complex(x, y)] = {}
     y += 1
 
 for i in a.keys():
     for j in a.keys():
         if i != j:
-            a[i].add(phase(i - j))
+            r, phi = polar(i - j)
+            if phi not in a[i]:
+                a[i][phi] = {}
+            a[i][phi][r] = j
 
-print(max([len(v) for (k, v) in a.items()]))
+x = max(a, key=(lambda k: len(a[k])))
+print(len(a[x]))
+
+x = a[x]
+i = 1
+max = 200
+
+while True:
+    for phi in sorted(x.keys(), key=rotate):
+        r = sorted(x[phi].keys())[0]
+
+        i += 1
+        if i > max:
+            print(x[phi][r].real * 100 + x[phi][r].imag)
+            break
+
+        del x[phi][r]
+        if len(x[phi]) == 0:
+            del x[phi]
+
+    if i > max or len(x) == 0:
+        break
