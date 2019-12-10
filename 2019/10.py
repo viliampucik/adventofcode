1d6e6879b91a0fc92acd19c4ba066691cc9d924d
Viliam Pucik <viliampucik@users.noreply.github.com>
2019-12-11 00:09:09 +0100

    simplified

--- 2019/10.py
+++ 2019/10.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python
 import fileinput
-from cmath import polar, pi
-
-
-def rotate(phi):  # +90 degrees rotation
-    phi -= 3 * pi / 2
-    if phi < -pi:
-        phi += 2 * pi
-    return phi
-
+from cmath import polar
 
 a = {}
-y = 0
 
-for line in fileinput.input():
+for y, line in enumerate(fileinput.input()):
     for x, v in enumerate(line.strip()):
         if v == "#":
             a[complex(x, y)] = {}
-    y += 1
 
 for i in a.keys():
     for j in a.keys():
         if i != j:
-            r, phi = polar(i - j)
+            r, phi = polar((j - i) / 1j)  # rotate 90 degrees
             if phi not in a[i]:
                 a[i][phi] = {}
             a[i][phi][r] = j
@@ -31,15 +21,14 @@ x = max(a, key=(lambda k: len(a[k])))
 print(len(a[x]))
 
 x = a[x]
-i = 1
-max = 200
+i = 200
 
 while True:
-    for phi in sorted(x.keys(), key=rotate):
+    for phi in sorted(x.keys()):
         r = sorted(x[phi].keys())[0]
 
-        i += 1
-        if i > max:
+        i -= 1
+        if i <= 0:
             print(x[phi][r].real * 100 + x[phi][r].imag)
             break
 
@@ -47,5 +36,5 @@ while True:
         if len(x[phi]) == 0:
             del x[phi]
 
-    if i > max or len(x) == 0:
+    if i <= 0 or len(x) == 0:
         break
