55d3dcca7ff77ef3b3f30772e8c1aef2a811ce74
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-02 22:54:57 +0100

    optimized

--- 2020/02.py
+++ 2020/02.py
@@ -4,23 +4,19 @@ import re
 from collections import Counter
 
 # 1-3 a: abcde
-p = re.compile(r"^(?P<min>\d+)-(?P<max>\d+) (?P<letter>.): (?P<password>.*)$")
+p = re.compile(r"^(\d+)-(\d+) (.): (.*)$")
 
 s1 = 0
 s2 = 0
 
 for line in fileinput.input():
-    d = p.match(line.strip()).groupdict()
-    min = int(d["min"])
-    max = int(d["max"])
+    low, high, letter, password = p.findall(line.strip())[0]
+    low, high = int(low), int(high)
 
-    c = Counter(d["password"]).get(d["letter"], 0)
-    if min <= c <= max:
+    if low <= Counter(password)[letter] <= high:
         s1 += 1
 
-    a = d["password"][min - 1]
-    b = d["password"][max - 1]
-    if a != b and (a == d["letter"] or b == d["letter"]):
+    if (password[low - 1] == letter) ^ (password[high - 1] == letter):
         s2 += 1
 
 print(s1)
