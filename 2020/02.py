aae6bb87b443c3faf7b6033b36056934924c40be
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-02 22:55:02 +0100

    optimized

--- 2020/02.py
+++ 2020/02.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import fileinput
 import re
-from collections import Counter
 
 # 1-3 a: abcde
 p = re.compile(r"^(\d+)-(\d+) (.): (.*)$")
@@ -13,7 +12,7 @@ for line in fileinput.input():
     low, high, letter, password = p.findall(line.strip())[0]
     low, high = int(low), int(high)
 
-    if low <= Counter(password)[letter] <= high:
+    if low <= password.count(letter) <= high:
         s1 += 1
 
     if (password[low - 1] == letter) ^ (password[high - 1] == letter):
