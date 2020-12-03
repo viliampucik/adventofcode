8270df988dddb9a2918509a06e407f22b0e2c7ca
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-03 06:54:03 +0100

    2020-03

---
+++ 2020/03.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import fileinput
+from math import prod
+
+
+def trees(r_init, d_init, m):
+    r, w, t = 0, len(m[0]), 0
+
+    for d in range(d_init, len(m)):
+        r += r_init
+        r %= w
+        t += m[d][r] == "#"
+
+    return t
+
+
+m = [l.strip() for l in fileinput.input()]
+print(trees(3, 1, m))
+print(prod(trees(*init, m)
+           for init in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]))
