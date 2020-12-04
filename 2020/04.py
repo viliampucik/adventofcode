4abd919e718277d5d5aab899bd78315675504330
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-04 22:18:29 +0100

    using more readable lambda approach, however it is slightly slower than compiled regexp

--- 2020/04.py
+++ 2020/04.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import sys
-import re
 
 fields = {
-    "byr": re.compile(r"^19[23456789]\d|200[012]$"),
-    "iyr": re.compile(r"^201\d|2020$"),
-    "eyr": re.compile(r"^202\d|2030$"),
-    "hgt": re.compile(r"^1[5678]\dcm|19[0123]cm|59in|6\din|7[0123456]in$"),
-    "hcl": re.compile(r"^#[\da-f]{6}$"),
-    "ecl": re.compile(r"^amb|blu|brn|gry|grn|hzl|oth$"),
-    "pid": re.compile(r"^\d{9}$"),
+    "byr": lambda x: 1920 <= int(x) <= 2002,
+    "iyr": lambda x: 2010 <= int(x) <= 2020,
+    "eyr": lambda x: 2020 <= int(x) <= 2030,
+    "hgt": lambda x: (x.endswith("cm") and 150 <= int(x[:-2]) <= 193) or
+                     (x.endswith("in") and 59 <= int(x[:-2]) <= 76),
+    "hcl": lambda x: re.fullmatch(r"#[\da-f]{6}", x),
+    "ecl": lambda x: x in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth"),
+    "pid": lambda x: re.fullmatch(r"\d{9}", x),
 }
 
 present = 0
@@ -22,8 +22,8 @@ for line in sys.stdin.read().split("\n\n"):
         continue
 
     present += 1
-    valid += all(data.match(passport[field])
-                       for field, data in fields.items())
+    valid += all(data(passport[field])
+                 for field, data in fields.items())
 
 print(present)
 print(valid)
