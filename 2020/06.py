8f1c8084bd0f9d89de3fcd853f66ffb777394282
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-06 14:23:48 +0100

    simplified

--- 2020/06.py
+++ 2020/06.py
@@ -6,7 +6,7 @@ s1 = s2 = 0
 for group in sys.stdin.read().split("\n\n"):
     s1 += len(set(group.replace("\n", "")))
     s2 += len(set.intersection(
-        *map(set, group.strip().split("\n"))
+        *map(set, group.split())
     ))
 
 print(s1)
