fc4e266adcec321c7d64ae225f3e4a964b3d1255
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-08 07:26:29 +0100

    increase readability

--- 2020/08.py
+++ 2020/08.py
@@ -2,39 +2,39 @@
 import fileinput
 
 
-def s(p, j=None):
-    a, i, v = 0, 0, set()
+def run(code, j=None):
+    accumulator, i, visited = 0, 0, set()
 
-    while i not in v and i < len(p):
-        v.add(i)
-        o, n = p[i]
+    while i not in visited and i < len(code):
+        visited.add(i)
+        operation, number = code[i]
 
-        if i == j:
-            o = "nop" if o == "jmp" else "jmp"
+        if i == j:  # the place to replace nop with jmp and vice versa
+            operation = "nop" if operation == "jmp" else "jmp"
 
-        if o == "acc":
-            a += n
+        if operation == "acc":
+            accumulator += number
             i += 1
-        elif o == "jmp":
-            i += n
+        elif operation == "jmp":
+            i += number
         else:  # nop
             i += 1
 
-    return a, i, v
+    return accumulator, i, visited
 
 
-p = []
+code = []
 
-for l in fileinput.input():
-    o, n = l.split()
-    p.append((o, int(n)))
+for line in fileinput.input():
+    operation, number = line.split()
+    code.append((operation, int(number)))
 
-a, _, v = s(p)
-print(a)
+accumulator, _, visited = run(code)
+print(accumulator)  # 1st part
 
-for j in v:
-    if p[j][0] in ("nop", "jmp"):
-        a, i, _ = s(p, j)
-        if i >= len(p):
-            print(a)
+for j in visited:
+    if code[j][0] in ("nop", "jmp"):
+        accumulator, i, _ = run(code, j)
+        if i >= len(code):
+            print(accumulator)  # 2nd part
             break
