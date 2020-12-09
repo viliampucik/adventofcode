c3f66631b3a8660c5da1bfe3ce46b175c4af507d
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-09 08:17:36 +0100

    2020-09

---
+++ 2020/09.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import fileinput
+from itertools import combinations
+
+n = [int(x) for x in fileinput.input()]
+
+invalid = None
+
+for i in range(25, len(n)):
+    if n[i] not in [sum(x) for x in combinations(n[i-25: i], 2)]:
+        invalid = n[i]
+        break
+
+print(invalid)
+
+start = stop = total = 0
+
+while total != invalid:
+    while total < invalid:
+        total += n[stop]
+        stop += 1
+    while total > invalid:
+        total -= n[start]
+        start += 1
+
+print(min(n[start:stop]) + max(n[start:stop]))
