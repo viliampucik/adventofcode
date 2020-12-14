a8e9778da946d2651836750b2195a36a32a9bd3a
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-14 23:55:32 +0100

    optimized

--- 2020/14.py
+++ 2020/14.py
@@ -13,22 +13,20 @@ def write(memory, mask, address, value):
 
 m1 = {}
 m2 = {}
-mask = None
+mask = and_mask = or_mask = not_mask = None
 
 for line in fileinput.input():
     key, value = line.strip().split(" = ")
     if key == "mask":
         mask = value
+        and_mask = int(mask.replace("1", "0").replace("X", "1"), 2)
+        or_mask = int(mask.replace("X", "0"), 2)
+        not_mask = ~and_mask
     else:
         address = int(key[4:-1])
         value = int(value)
-
-        m1[address] = value \
-            & int(mask.replace("1", "0").replace("X", "1"), 2) \
-            | int(mask.replace("X", "0"), 2)
-
-        address &= int(mask.replace("0", "1").replace("X", "0"), 2)
-        write(m2, mask, address, value)
+        m1[address] = value & and_mask | or_mask
+        write(m2, mask, address & not_mask, value)
 
 print(sum(m1.values()))
 print(sum(m2.values()))
