87ac0a81500db4403a6b8dca9a9b2e538bc08ee3
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-15 22:49:42 +0100

    optimized

--- 2020/14.py
+++ 2020/14.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python
 import fileinput
+from itertools import chain, combinations
 
 
-def write(memory, mask, address, value):
-    if "X" in mask:
-        i = mask.index("X")
-        write(memory, mask[:i] + "0" + mask[i+1:], address, value)
-        write(memory, mask[:i] + "1" + mask[i+1:], address, value)
-    else:
-        memory[int(mask, 2) | address] = value
+def powerset(s):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 
 m1 = {}
 m2 = {}
-mask = and_mask = or_mask = not_mask = None
+mask = mask_and = mask_or = mask_not = offsets = None
 
 for line in fileinput.input():
     key, value = line.strip().split(" = ")
     if key == "mask":
         mask = value
-        and_mask = int(mask.replace("1", "0").replace("X", "1"), 2)
-        or_mask = int(mask.replace("X", "0"), 2)
-        not_mask = ~and_mask
+        mask_and = int(mask.replace("1", "0").replace("X", "1"), 2)
+        mask_or = int(mask.replace("X", "0"), 2)
+        mask_not = ~mask_and
+        offsets = [
+            sum(x)
+            for x in powerset(
+                [1 << (35 - i) for i, b in enumerate(mask) if b == "X"]
+            )
+        ]
     else:
         address = int(key[4:-1])
         value = int(value)
-        m1[address] = value & and_mask | or_mask
-        write(m2, mask, address & not_mask, value)
+
+        m1[address] = value & mask_and | mask_or
+
+        address &= mask_not
+        for offset in offsets:
+            m2[address | offset] = value
 
 print(sum(m1.values()))
 print(sum(m2.values()))
