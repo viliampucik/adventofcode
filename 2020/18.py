28ad5cbb4dc9ea18edf1e5de19d8e2019c69beaa
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-18 23:20:27 +0100

    more hacks

--- 2020/18.py
+++ 2020/18.py
@@ -6,13 +6,11 @@ import sys
 class I(int):
     def __add__(a, b): return I(a.real + b.real)
     def __sub__(a, b): return I(a.real * b.real)
-    def __mul__(a, b): return I(a.real + b.real)
+    __mul__ = __add__
 
 
-lines = [
-    re.sub(r"(\d+)", r"I(\1)", line.replace("*", "-"))
-    for line in sys.stdin.read().splitlines()
-]
-
+lines = re.sub(
+    r"(\d+)", r"I(\1)", sys.stdin.read()
+).replace("*", "-").splitlines()
 print(sum(eval(line) for line in lines))
 print(sum(eval(line.replace("+", "*")) for line in lines))
