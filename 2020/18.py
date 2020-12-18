c970de55805a59d1651f294b8a15c7270fc113f6
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-18 23:08:00 +0100

    hack

--- 2020/18.py
+++ 2020/18.py
@@ -1,51 +1,18 @@
 #!/usr/bin/env python
 import re
 import sys
-from operator import add, mul
 
 
-def same_precedence(line):
-    a, *values = line.split()
-    a, ivalues = int(a), iter(values)
-
-    for o, b in zip(ivalues, ivalues):
-        b = int(b)
-        a = add(a, b) if o == "+" else mul(a, b)
-
-    return a
+class I(int):
+    def __add__(a, b): return I(a.real + b.real)
+    def __sub__(a, b): return I(a.real * b.real)
+    def __mul__(a, b): return I(a.real + b.real)
 
 
-def add_before_mul(line):
-    regexp = re.compile(r"\d+ \+ \d+")
-    changes = 1
+lines = [
+    re.sub(r"(\d+)", r"I(\1)", line.replace("*", "-"))
+    for line in sys.stdin.read().splitlines()
+]
 
-    while changes:
-        line, changes = regexp.subn(
-            lambda m: str(eval(m.group())),
-            line
-        )
-
-    return eval(line)
-
-
-def solve(lines, func):
-    regexp = re.compile(r"\([^()]+\)")
-    total = 0
-
-    for line in lines:
-        changes = 1
-
-        while changes:
-            line, changes = regexp.subn(
-                lambda m: str(func(m.group()[1:-1])),
-                line
-            )
-
-        total += func(line)
-
-    return total
-
-
-lines = sys.stdin.read().splitlines()
-print(solve(lines, same_precedence))
-print(solve(lines, add_before_mul))
+print(sum(eval(line) for line in lines))
+print(sum(eval(line.replace("+", "*")) for line in lines))
