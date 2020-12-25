b050d7932ed3f4bcff37415efe2e5405d8aee2d0
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-25 15:30:32 +0100

    optimized

--- 2020/25.py
+++ 2020/25.py
@@ -4,18 +4,14 @@ import sys
 keys = list(map(int, sys.stdin.read().splitlines()))
 subject = 7
 value = loop = 1
-transform = lambda value, subject: (value * subject) % 20201227
+modulo = 20201227
 
 while True:
-    value = transform(value, subject)
+    value = (value * subject) % modulo
     if value in keys:
         break
     loop += 1
 
 subject = keys[keys.index(value) - 1]  # use the next key
-value = 1
 
-for _ in range(loop):
-    value = transform(value, subject)
-
-print(value)
+print(pow(subject, loop, modulo))
