c8fb996194d99c1115db64d45e4e187f984491ce
Viliam Pucik <viliampucik@users.noreply.github.com>
2020-12-25 22:45:11 +0100

    including faster alternatives

--- 2020/01.py
+++ 2020/01.py
@@ -3,6 +3,7 @@ import fileinput
 from math import prod
 from itertools import combinations
 
+# Slower, but works in case of "n" containing duplicates or single 1010
 
 def solve(length):
     for c in combinations(n, length):
@@ -13,3 +14,24 @@ def solve(length):
 n = list(map(int, fileinput.input()))
 print(solve(2))
 print(solve(3))
+
+# Faster alternatives, but could lead into false positives if "n" contains duplicates or single 1010
+
+def two(n):
+    s = set(n)
+    for x in s:
+        if (y := 2020 - x) in s:
+            return x * y
+
+
+def tree(n):
+    s = set(n)
+    for i, x in enumerate(n):
+        yz = 2020 - x
+        for y in n[i + 1:]:
+            if (z := yz - y) in s:
+                return x * y * z
+
+
+print(two(n))
+print(tree(n))
