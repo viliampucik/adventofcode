1a9742b4e798fd83f4fd16b6343e22f7e1c2590f
Viliam Pucik <viliampucik@users.noreply.github.com>
2021-01-01 20:36:51 +0100

    simplified

--- 2015/01.py
+++ 2015/01.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
-import fileinput
-
 floor = 0
 basement = None
 
-for i, c in enumerate(next(fileinput.input()).strip()):
-    if c == "(":
-        floor += 1
-    else:  # c == ")"
-        floor -= 1
+for i, c in enumerate(input()):
+    floor += 1 if c == "(" else -1
     if floor == -1 and basement is None:
         basement = i + 1
 
