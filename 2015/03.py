4ff22c413755660ff107f15ed66469f300fe1eff
Viliam Pucik <viliampucik@users.noreply.github.com>
2021-01-02 18:59:53 +0100

    increase readability

--- 2015/03.py
+++ 2015/03.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-o = {"^": 1j, "v": -1j, ">": 1, "<": -1}
+orientation = {"^": 1j, "v": -1j, ">": 1, "<": -1}
 location, locations = 0, [0, 0]
 houses1, houses2 = set((location,)), set(locations)
 
 for i, c in enumerate(input()):
-    location += o[c]
+    location += orientation[c]
     houses1.add(location)
 
-    locations[i % 2] += o[c]
-    houses2.update(locations)
+    locations[(j := i % 2)] += orientation[c]
+    houses2.add(locations[j])
 
 print(len(houses1))
 print(len(houses2))
