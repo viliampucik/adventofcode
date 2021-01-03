b0de59c968b070613d31ddd96d863081461c2158
Viliam Pucik <viliampucik@users.noreply.github.com>
2021-01-03 20:15:15 +0100

    2015-08

---
+++ 2015/08.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+
+s1 = s2 = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    s1 += len(line) - len(eval(line))
+    s2 += 2 + line.count('\\') + line.count('"')
+
+print(s1)
+print(s2)
