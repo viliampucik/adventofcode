9c19e69c5d5f3b8ff252a05a25e98017fbb027f2
Viliam Pucik <viliampucik@users.noreply.github.com>
2021-12-06 00:04:09 +0100

    2021-04

---
+++ 2021/04.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from itertools import chain
+import sys
+
+numbers, *data = sys.stdin.read().strip().split("\n\n")
+
+boards = []
+for board in data:
+    # fmt:off
+    rows = [
+        [int(x) for x in row.split()]
+        for row in board.split("\n")
+    ]
+    # fmt:on
+    boards.append([set(line) for line in chain(rows, zip(*rows))])
+
+drawn, remaining = set(), set(range(len(boards)))
+
+for number in map(int, numbers.split(",")):
+    drawn.add(number)
+
+    for i in set(remaining):
+        if any(line <= drawn for line in boards[i]):
+            remaining.remove(i)
+
+            if len(remaining) == len(boards) - 1 or not remaining:
+                print(number * sum(set.union(*boards[i]) - drawn))
