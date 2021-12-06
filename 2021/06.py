31af9e2717e433a462262a0eb32e5ff4f7793a67
Viliam Pucik <viliampucik@users.noreply.github.com>
2021-12-06 20:12:55 +0100

    simplified

--- 2021/06.py
+++ 2021/06.py
@@ -9,6 +9,6 @@ for i in map(int, sys.stdin.read().split(",")):
 
 for part in (80, 256 - 80):
     for _ in range(part):
-        d.append(d.popleft())
+        d.rotate(-1)
         d[6] += d[-1]
     print(sum(d))
