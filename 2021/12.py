802a7f1395c7e1ce46d225bb18d2e4c3260f9dae
Viliam Pucik <viliampucik@users.noreply.github.com>
2021-12-12 08:31:39 +0100

    2021-12

---
+++ 2021/12.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from collections import defaultdict, deque
+import fileinput
+
+caves = defaultdict(list)
+
+for line in fileinput.input():
+    a, b = line.strip().split("-")
+    caves[a].append(b)
+    caves[b].append(a)
+
+for skip_duplicates in True, False:
+    count, search = 0, deque((child, set(), False) for child in caves["start"])
+
+    while search:
+        parent, lowers, duplicate = search.popleft()
+
+        if parent == "end":
+            count += 1
+            continue
+        elif parent.islower():
+            if parent in lowers:
+                if skip_duplicates or duplicate:
+                    continue
+                else:
+                    duplicate = True
+            lowers.add(parent)
+
+        search.extend((child, set(lowers), duplicate) for child in caves[parent] if child != "start")
+
+    print(count)
