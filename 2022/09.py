fada98d129b57d7d0bddbb18ed0d51e29c73c999
Viliam Pucik <viliampucik@users.noreply.github.com>
2022-12-12 08:57:24 +0100

    simplified

--- 2022/09.py
+++ 2022/09.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
-d = {"U": +1j, "D": -1j, "L": -1, "R": +1}
-rope = [0 + 0j] * 10
-s1, s2 = {rope[1]}, {rope[-1]}
+dirs = {"U": 1j, "D": -1j, "L": -1, "R": 1}
+sign = lambda a: (a > 0) - (a < 0)
+rope, s1, s2 = [0] * 10, {0}, {0}
 
 for line in open(0).read().splitlines():
-    dir, steps = line.split()
+    d, s = line.split()
 
-    for _ in range(int(steps)):
-        for i in range(len(rope)):
-            if i == 0:
-                rope[i] += d[dir]
-                continue
+    for _ in range(int(s)):
+        rope[0] += dirs[d]
 
+        for i in range(1, len(rope)):
             if abs(diff := rope[i - 1] - rope[i]) >= 2:
-                if (a := abs(diff.real)) > 0:
-                    rope[i] += complex(diff.real / a, 0)
-                if (a := abs(diff.imag)) > 0:
-                    rope[i] += complex(0, diff.imag / a)
+                rope[i] += complex(sign(diff.real), sign(diff.imag))
 
         s1.add(rope[1])
         s2.add(rope[-1])
