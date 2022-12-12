907fc002327e2d80bfc7c21eea24e424d28eed60
Viliam Pucik <viliampucik@users.noreply.github.com>
2022-12-12 08:57:24 +0100

    optimized

--- 2022/06.py
+++ 2022/06.py
@@ -5,3 +5,19 @@ for size in (4, 14):
         if len(set(data[i - size : i])) == size:
             print(i)
             break
+
+# Less elegant, but way faster than "set()" approach
+for size in (4, 14):
+    distance, last_seen = size, {}
+
+    for i, c in enumerate(data):
+        if distance >= (d := i - last_seen.get(c, i - size)):
+            distance = d
+        else:
+            distance += 1
+
+        if distance >= size and i >= size - 1:
+            print(i + 1)
+            break
+
+        last_seen[c] = i
