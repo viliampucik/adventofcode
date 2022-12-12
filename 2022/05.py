41320a1c90a7274c81370bf60ff5ab935dfede3e
Viliam Pucik <viliampucik@users.noreply.github.com>
2022-12-12 08:57:24 +0100

    2022-05

---
+++ 2022/05.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+def solve(stacks, lines, step=1):
+    for count, src, dst in lines:
+        stacks[dst] += stacks[src][-count:][::step]
+        stacks[src] = stacks[src][:-count]
+
+    return "".join(s[-1] for s in stacks)
+
+
+data, lines = open(0).read().split("\n\n")
+# fmt:off
+stacks = [
+    "".join(column).rstrip()
+    for i, column in enumerate(zip(*data.splitlines()[-2::-1]))
+	if i % 4 == 1
+]
+lines = [
+    (int(line[1]), int(line[3]) - 1, int(line[5]) - 1)
+    for line in map(str.split, lines.splitlines())
+]
+# fmt:on
+print(solve(stacks.copy(), lines, -1))
+print(solve(stacks.copy(), lines))
