25736e6ea8fc76938b270684a0f5edf7e08a1a51
Viliam Pucik <viliampucik@users.noreply.github.com>
2022-12-12 08:57:24 +0100

    2022-11

---
+++ 2022/11.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from collections import deque
+from math import gcd, prod
+from operator import floordiv, mod
+
+ITEMS, OPER, TEST, A, B = range(5)
+
+
+def solve(monkeys, iterations, op, number):
+    inspections = [0] * len(monkeys)
+
+    for _ in range(iterations):
+        for i, m in enumerate(monkeys):
+            while m[ITEMS]:
+                inspections[i] += 1
+                item = op(m[OPER](m[ITEMS].popleft()), number)
+                if item % m[TEST] == 0:
+                    monkeys[m[A]][ITEMS].append(item)
+                else:
+                    monkeys[m[B]][ITEMS].append(item)
+
+    return prod(sorted(inspections)[-2:])
+
+
+monkeys = [
+    [
+        deque(map(int, item[ITEMS].replace(",", "").split()[2:])),
+        eval("lambda old: " + item[OPER].split(" = ")[-1]),
+        int(item[TEST].split()[-1]),
+        int(item[A].split()[-1]),
+        int(item[B].split()[-1]),
+    ]
+    for block in open(0).read().split("\n\n")
+    for item in [block.splitlines()[1:]]
+]
+
+print(solve(monkeys, 20, floordiv, 3))
+tests = [m[TEST] for m in monkeys]
+print(solve(monkeys, 10_000, mod, prod(tests) // gcd(*tests)))  # Use least common multiple
