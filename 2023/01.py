eb412b01bc8315fe8a42a718493e2d64d9c1073b
Viliam Pucik <Viliam.Pucik@external.erstegroup.com>
2023-12-14 11:39:13 +0100

    simplified

--- 2023/01.py
+++ 2023/01.py
@@ -4,13 +4,20 @@ import re
 lines = open(0).read().splitlines()
 # fmt:off
 print(sum(
-    next(int(i) for i in line if i.isdigit()) * 10 +
-    next(int(i) for i in reversed(line) if i.isdigit())
+    x[0] * 10 + x[-1]
     for line in lines
+    for x in [[int(i) for i in line if i.isdigit()]]
 ))
 
+# Alternative and probably faster solution
+# print(sum(
+#     next(int(i) for i in line if i.isdigit()) * 10 +
+#     next(int(i) for i in reversed(line) if i.isdigit())
+#     for line in lines
+# ))
+
 digits = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
-r = re.compile(f"(?=(\d|{ '|'.join(digits.keys()) }))")
+r = re.compile(rf"(?=({ '|'.join(digits.keys()) }))")
 
 print(sum(
     digits[x[0]] * 10 + digits[x[-1]]
