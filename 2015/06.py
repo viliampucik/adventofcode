1252e4b67d6acd9024fdb4d739ed820d7d292fe1
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:45 +0100

    optimized

--- 2015/06.py
+++ 2015/06.py
@@ -1,34 +1,29 @@
-#!/usr/bin/env python
-import fileinput
+#!/usr/bin/env pypy3
 import re
+import sys
+from array import array
 
+r = re.compile(r"(.+) (\d+),(\d+) through (\d+),(\d+)")
+lights1 = array("B", [0] * 1_000_000)
+lights2 = array("I", [0] * 1_000_000)
 
-def lights(instructions, brightness=False):
-    grid = [[0 for _ in range(1000)] for _ in range(1000)]
+for line in sys.stdin:
+    action, *numbers = r.match(line).groups()
+    col_min, row_min, col_max, row_max = map(int, numbers)
 
-    for mode, fx, fy, tx, ty in instructions:
-        for i in range(fx, tx + 1):
-            for j in range(fy, ty + 1):
-                if mode == "turn on":
-                    grid[i][j] = grid[i][j] + 1 if brightness else 1
-                elif mode == "turn off":
-                    grid[i][j] = max(0, grid[i][j] - 1) if brightness else 0
-                else:  # toggle
-                    if brightness:
-                        grid[i][j] += 2
-                    else:
-                        grid[i][j] = 0 if grid[i][j] == 1 else 1
+    for row in range(row_min, row_max + 1):
+        for col in range(col_min, col_max + 1):
+            bulb = row * 1000 + col
+            if action == "turn on":
+                lights1[bulb] = 1
+                lights2[bulb] += 1
+            elif action == "turn off":
+                lights1[bulb] = 0
+                if lights2[bulb] > 0:
+                    lights2[bulb] -= 1
+            else:  # action == "toggle":
+                lights1[bulb] = 0 if lights1[bulb] else 1
+                lights2[bulb] += 2
 
-    return sum([sum(row) for row in grid])
-
-
-instructions = []
-
-for line in fileinput.input():
-    mode, fx, fy, tx, ty = re.match(
-        r"(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)", line,
-    ).groups()
-    instructions.append((mode, int(fx), int(fy), int(tx), int(ty)))
-
-print(lights(instructions))
-print(lights(instructions, True))
+print(sum(lights1))
+print(sum(lights2))
