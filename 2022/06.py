c4eaa0eed31d9aee560de947be0a5992254e3058
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:46 +0100

    simplified

--- 2022/06.py
+++ 2022/06.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
-def solve(data, size):
+data = input()
+for size in (4, 14):
     for i in range(size, len(data)):
         if len(set(data[i - size : i])) == size:
-            return i
-
-
-print(solve(data := input(), 4))
-print(solve(data, 14))
+            print(i)
+            break
