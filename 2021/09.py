835b5e7f0ab3c8174644830774a7387e2ace75fa
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:45 +0100

    2021-09

---
+++ 2021/09.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from math import prod
+import fileinput
+
+m = [list(map(int, line.strip())) for line in fileinput.input()]
+h, w, part1, part2 = len(m), len(m[0]), 0, []
+
+for r in range(h):
+    for c in range(w):
+        # fmt:off
+        if any(
+            m[r][c] >= m[x][y]
+            for i, j in ((-1, 0), (1, 0), (0, -1), (0, 1))
+            if 0 <= (x := r + i) < h and 0 <= (y := c + j) < w
+        ):
+            continue
+        # fmt:on
+        part1 += m[r][c] + 1
+
+        visited, visiting = set(), set([(r, c)])
+
+        while visiting:
+            a, b = visiting.pop()
+            visited.add((a, b))
+
+            for i, j in (-1, 0), (1, 0), (0, -1), (0, 1):
+                # fmt:off
+                if 0 <= (x := a + i) < h and 0 <= (y := b + j) < w \
+                    and m[x][y] < 9 \
+                    and (x, y) not in visited:
+                    visiting.add((x, y))
+                # fmt:on
+
+        part2.append(len(visited))
+
+print(part1)
+print(prod(sorted(part2)[-3:]))
