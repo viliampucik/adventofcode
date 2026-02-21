5d7c4843f564d0fd42e980be678bff93c1939ed7
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:45 +0100

    2015-09

---
+++ 2015/09.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+from collections import defaultdict
+from itertools import permutations
+
+cities = defaultdict(dict)
+
+for line in sys.stdin:
+    city_from, _, city_to, _, distance = line.strip().split()
+    cities[city_from][city_to] = int(distance)
+    cities[city_to][city_from] = int(distance)
+
+paths = [
+    sum(
+        cities[city_from][city_to]
+        for city_from, city_to in zip(locations, locations[1:])
+    )
+    for locations in permutations(cities)
+]
+
+print(min(paths))
+print(max(paths))
