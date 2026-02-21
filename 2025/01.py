453bff8911a78fb8d5c85d275ff24b6d2d6502b9
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:50:01 +0100

    simplified

--- 2025/01.py
+++ 2025/01.py
@@ -4,8 +4,7 @@ p, s1, s2 = 50, 0, 0
 
 for number in (-int(line[1:]) if line[0] == 'L' else int(line[1:]) for line in open(0)):
   r, p = divmod(p + number, 100)
-  if p == 0:
-    s1 += 1
+  s1 += p == 0
   s2 += abs(r)
 
 print(s1, s2)
