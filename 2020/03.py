6a5fa5acc7ab1b1fe7d07d40e7ffcf90056371d6
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:45 +0100

    including faster alternative

--- 2020/03.py
+++ 2020/03.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import fileinput
 from math import prod
+from itertools import count
 
 
 def trees(r_init, d_init, m):
@@ -17,4 +18,20 @@ def trees(r_init, d_init, m):
 m = [l.strip() for l in fileinput.input()]
 print(trees(3, 1, m))
 print(prod(trees(*init, m)
+           for init in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]))
+
+# Faster alternative
+
+def toboggan(m, right, down):
+    return sum(
+        m[row][col % len(m[row])] == "#"
+        for row, col in zip(
+            range(0, len(m), down),
+            count(0, right)
+        )
+    )
+
+
+print(toboggan(m, 3, 1))
+print(prod(toboggan(m, *init)
            for init in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]))
