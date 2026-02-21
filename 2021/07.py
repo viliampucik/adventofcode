7d6c34e2c163fca3c5f05fe9ed5da907267fe1bb
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:45 +0100

    simplified

--- 2021/07.py
+++ 2021/07.py
@@ -5,7 +5,7 @@ import sys
 c = Counter(map(int, sys.stdin.read().split(",")))
 h_min, h_max = min(c), max(c)
 
-for p in (abs, lambda x: ((y := abs(x)) * (y + 1)) // 2):
+for p in (abs, lambda x: (y := abs(x)) * (y + 1) // 2):
     # fmt:off
     print(min(
         sum(
