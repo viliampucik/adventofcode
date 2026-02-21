7b8753aaefb8d3772e032e8432ed710514cf4e39
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:45 +0100

    2021-02

---
+++ 2021/02.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import fileinput
+
+aim, h, d = 0, 0, 0
+
+for l in fileinput.input():
+    cmd, x = l.strip().split(" ")
+    x = int(x)
+    # fmt: off
+    if cmd == "down": aim += x
+    elif cmd == "up": aim -= x
+    else: h += x; d += aim * x  # cmd == "forward"
+    # fmt: on
+print(h * aim)
+print(h * d)
