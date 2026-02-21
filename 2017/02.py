7c8980f1b106ec1b68c8db247e9bcd5f812139c4
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:46 +0100

    2017-02

---
+++ 2017/02.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# fmt:off
+s = [
+    [int(i) for i in l.split()]
+    for l in open(0).readlines()
+]
+print(sum(max(r) - min(r) for r in s))
+print(sum(
+    a // b
+    for r in s
+    for a in r
+    for b in r
+    if a != b and a % b == 0
+))
