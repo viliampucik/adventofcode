ce390cab218de82d0ca3f9e4d8ccdf6069d7d5f2
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:44 +0100

    optimized

--- 2020/19.py
+++ 2020/19.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 import sys
-import re
-from lark import Lark
+import regex
 
-rules, messages = sys.stdin.read().split("\n\n")
+
+def solve(rules, messages):
+    def expand(value):
+        if not value.isdigit(): return value
+        return "(?:" + "".join(map(expand, rules[value].split())) + ")"
+
+    r = regex.compile(expand("0"))
+    return sum(r.fullmatch(m) is not None for m in messages)
+
+
+raw_rules, messages = sys.stdin.read().split("\n\n")
 messages = messages.splitlines()
-
-
-def solve(rules):
-    parser = Lark(re.sub(r"(\d+)", r"t\1", rules), start="t0")
-    valid = len(messages)
-    for message in messages:
-        try:
-            parser.parse(message)
-        except:
-            valid -= 1
-    print(valid)
-
-
-solve(rules)
-solve(rules
-    .replace("8: 42", "8: 42 | 42 8")
-    .replace("11: 42 31", "11: 42 31 | 42 11 31")
+rules = dict(
+    raw_rule.replace('"', "").split(": ", 1)
+    for raw_rule in raw_rules.splitlines()
 )
+
+print(solve(rules, messages))
+rules["8"] = "42 +"  # repeat pattern
+rules["11"] = "(?P<R> 42 (?&R)? 31 )"  # recursive pattern
+print(solve(rules, messages))
