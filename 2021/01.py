e3ddc653aad34ce7c5109841bdecd4193ebc14bb
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:45 +0100

    simplified

--- 2021/01.py
+++ 2021/01.py
@@ -2,5 +2,5 @@
 import fileinput
 
 n = list(map(int, fileinput.input()))
-print(sum(a < b for a, b in zip(n, n[1:])))
-print(sum(a < b for a, b in zip(n, n[3:])))  # n[1:] and n[2:] would be repeated on both sides of the comparison statement
+for i in 1, 3:
+    print(sum(a < b for a, b in zip(n, n[i:])))
