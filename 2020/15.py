dc1b3aab32f2be15f28eaae87e5f1d3507fff998
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:45 +0100

    optimized

--- 2020/15.py
+++ 2020/15.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env pypy3
 import sys
+from array import array
 
 numbers = [int(i) for i in next(sys.stdin).split(",")]
 last = numbers[-1]
-age = {number: i+1 for i, number in enumerate(numbers)}
+size = 30_000_000
+# age = {number: i+1 for i, number in enumerate(numbers)}  # slow dict alternative
+# Despite more memory consumption the preallocated list approach is faster than dict
+age = [0] * size
+for i, number in enumerate(numbers):
+    age[number] = i + 1
 
-for i in range(len(numbers), 30_000_000):
+for i in range(len(numbers), size):
     if i == 2020: print(last)
-    age[last], last = i, i - age.get(last, i)
+    # age[last], last = i, i - age.get(last, i)  # slow dict alternative
+    previous = age[last]
+    age[last], last = i, i - previous if previous else 0
 
 print(last)
