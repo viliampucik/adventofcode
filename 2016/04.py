7b2e5c2269766bb14d3a86577756ca5b643db8c2
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:43 +0100

    2016-04

---
+++ 2016/04.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import re
+import fileinput
+from collections import Counter
+
+
+def descrypt(c, r):
+    if c == "-":
+        return " "
+
+    return chr((ord(c) - ord("a") + r) % 26 + ord("a"))
+
+
+s = 0
+nos = None
+
+for line in fileinput.input():
+    name, sector, checksum = re.match(
+        r"^(.*)-(\d+)\[(.+)\]$", line.strip()
+    ).groups()
+    sector = int(sector)
+
+    if "northpole" in "".join([descrypt(c, sector) for c in name]):
+        nos = sector
+
+    name = "".join(sorted(name.replace("-", "")))
+    room = "".join([i[0] for i in Counter(name).most_common(len(checksum))])
+
+    if room == checksum:
+        s += sector
+
+print(s)
+print(nos)
