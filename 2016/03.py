03514c52c02f0ec95fa254f90a5ce8f5e5896229
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-21 18:25:43 +0100

    2016-03

---
+++ 2016/03.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import fileinput
+
+count = 0
+l = [[], [], []]
+
+for line in fileinput.input():
+    a, b, c = [int(i) for i in line.strip().split()]
+    if a + b > c and a + c > b and b + c > a:
+        count += 1
+    l[0].append(a)
+    l[1].append(b)
+    l[2].append(c)
+
+print(count)
+
+count = 0
+
+for i in l:
+    for j in range(0, len(i), 3):
+        a, b, c = i[j:j+3]
+        if a + b > c and a + c > b and b + c > a:
+            count += 1
+
+print(count)
