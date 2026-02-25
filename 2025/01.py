c38102dcb4fce673ff88fa83c9bf49e5b33ee2b4
Viliam Pucik <672009+viliampucik@users.noreply.github.com>
2026-02-25 23:05:15 +0100

    formatting

--- 2025/01.py
+++ 2025/01.py
@@ -2,9 +2,9 @@
 
 p, s1, s2 = 50, 0, 0
 
-for number in (-int(line[1:]) if line[0] == 'L' else int(line[1:]) for line in open(0)):
-  r, p = divmod(p + number, 100)
-  s1 += p == 0
-  s2 += abs(r)
+for number in (-int(line[1:]) if line[0] == "L" else int(line[1:]) for line in open(0)):
+    r, p = divmod(p + number, 100)
+    s1 += p == 0
+    s2 += abs(r)
 
 print(s1, s2)
